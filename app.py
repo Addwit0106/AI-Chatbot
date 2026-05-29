8cc228fbda0ffd54b369837236fe55ab829ac251
Addwit0106 <addwitagrawal01@gmail.com>
2026-05-29 15:11:18 +0530

    Added PDF upload functionality

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 
 from flask import Flask, render_template, request
 import requests
+import os
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 
-
+# Chat Memory
 conversation_history = []
 
 
@@ -20,11 +22,13 @@ def chat():
 
     user_message = request.json["message"]
 
-    conversation_history.append(f"User: {user_message}")
+    # Save user message
+    conversation_history.append(
+        f"User: {user_message}"
+    )
 
-
+    # Keep last few messages
     recent_history = conversation_history[-4:]
-
 
     full_prompt = f"""
 You are a helpful AI assistant.
@@ -41,12 +45,11 @@ Assistant:
 """
 
     data = {
-        "model": "phi3",
+        "model": "gemma:2b",
         "prompt": full_prompt,
         "stream": False,
-        
         "options": {
-        "num_predict": 200
+            "num_predict": 200
         }
     }
 
@@ -65,15 +68,52 @@ Assistant:
             "No response from AI."
         )
 
-    except Exception as e:
+    except Exception:
 
         ai_response = "AI is busy or not responding."
 
-
-    conversation_history.append(f"Assistant: {ai_response}")
+    # Save AI response
+    conversation_history.append(
+        f"Assistant: {ai_response}"
+    )
 
     return ai_response
 
 
+@app.route("/upload-pdf", methods=["POST"])
+def upload_pdf():
+
+    if "pdf" not in request.files:
+        return "No PDF uploaded."
+
+    pdf_file = request.files["pdf"]
+
+    if pdf_file.filename == "":
+        return "No file selected."
+
+    upload_folder = "uploads"
+
+    os.makedirs(
+        upload_folder,
+        exist_ok=True
+    )
+
+    filepath = os.path.join(
+        upload_folder,
+        pdf_file.filename
+    )
+
+    pdf_file.save(filepath)
+
+    reader = PdfReader(filepath)
+
+    total_pages = len(
+        reader.pages
+    )
+
+    return f"PDF uploaded successfully! Pages: {total_pages}"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
